5064072388334c47ea44a463f30cfb1601acc83e
Even <cbhrat50@gmail.com>
2023-09-11 15:59:19 +0530

    Create HomePage Sections

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -6,11 +6,14 @@ import { headingType } from "../../components/ChangableHeadings/ChangableHeading
 import FullNav from "../../components/Navigation/FullNav";
 import MobileNav from "../../components/Navigation/MobileNav/MobileNav";
 import Lottie from "lottie-react";
+import MoveToElementButton from "../../components/MoveToElementButton/MoveToElementButton";
+import useDisplayRes from "../../hooks/useElementToDisplayHeight";
 
 // Animtaion Jsons
 import menWithTech from "../../assets/manWithTech.json";
-import MoveToElementButton from "../../components/MoveToElementButton/MoveToElementButton";
-import useDisplayRes from "../../hooks/useElementToDisplayHeight";
+import visionAnimation from "../../assets/vision-animation.json";
+import liveAnimation from "../../assets/future-animation.json";
+import JoinAnimation from "../../assets/join-animation.json";
 
 const ChangableHeadingsData: {
   headingType: headingType;
@@ -34,6 +37,9 @@ const HomePage = () => {
   const [isMobileNav, setMobileNav] = useState(false);
   const headerRef = useRef<HTMLElement>(null);
   const AboutHoiRef = useRef<HTMLElement>(null);
+  const VisionRef = useRef<HTMLElement>(null);
+  const ApartRef = useRef<HTMLElement>(null);
+  const JoinRef = useRef<HTMLElement>(null);
 
   useEffect(() => {
     screen.width < 900 && setMobileNav(true);
@@ -43,6 +49,9 @@ const HomePage = () => {
 
   if (!isMobileNav) {
     useDisplayRes(AboutHoiRef.current);
+    useDisplayRes(VisionRef.current);
+    useDisplayRes(ApartRef.current);
+    useDisplayRes(JoinRef.current);
   }
 
   useEffect(() => {
@@ -111,6 +120,68 @@ const HomePage = () => {
           </div>
           <div className="w-full lg:w-2/5">
             <Lottie animationData={menWithTech} />
+          </div>
+        </section>
+
+        <section
+          ref={VisionRef}
+          className="flex-col-reverse h-auto pt-20 lg:pt-5 lg:pl-16 lg:pr-16 overflow-hidden  flex lg:flex-row-reverse justify-center items-center text-center bg-main-400"
+        >
+          <div className="animation-with-text flex flex-col mb-20 lg:mb-0 lg:w-3/5 justify-center w-full  lg:justify-evenly items-center">
+            <h2 className=" uppercase font-serif text-center w-full  mb-8 mt-10 lg:mt-0 text-white-400 text-4xl">
+              Our Vision
+            </h2>
+            <p className="p-4 font-sans font-white leading-9 text-white-400  text-2xl">
+              At HoI, our vision is clear: We believe that the world can be a
+              more exciting, efficient, and interconnected place through
+              innovative hardware and cutting-edge software. Our mission is to
+              make this vision a reality by exploring the endless possibilities
+              of AI and IoT technologies.
+            </p>
+          </div>
+          <div className="w-full lg:w-2/5">
+            <Lottie animationData={visionAnimation} />
+          </div>
+        </section>
+
+        <section
+          ref={ApartRef}
+          className="apart flex-col-reverse h-auto pt-20 lg:pt-5 lg:pl-16 lg:pr-16 overflow-hidden  flex lg:flex-row justify-center items-center text-center bg-bg-400 "
+        >
+          <div className="animation-with-text flex flex-col mb-20 lg:mb-0 lg:w-3/5 justify-center w-full  lg:justify-evenly items-center">
+            <h2 className=" uppercase font-serif text-center w-full   mb-12 text-white-400 text-4xl">
+              What Sets Us Apart
+            </h2>
+            <p className="p-4 font-sans font-white leading-9 text-white-400  text-2xl">
+              At HoI, we don't just talk about innovation; we live it. Our team
+              of experts is always on the forefront of technology trends, eager
+              to create hardware and software that not only works but also
+              amazes. We're not just building the future; we're shaping it.
+            </p>
+          </div>
+          <div className="w-full lg:w-2/5">
+            <Lottie animationData={liveAnimation} />
+          </div>
+        </section>
+
+        <section
+          ref={JoinRef}
+          className="flex-col-reverse h-auto pt-20 lg:pt-5 lg:pl-16 lg:pr-16 overflow-hidden  flex lg:flex-row-reverse justify-center items-center text-center bg-main-400"
+        >
+          <div className="animation-with-text flex flex-col mb-20 lg:mb-0 lg:w-3/5 justify-center w-full  lg:justify-evenly items-center">
+            <h2 className=" uppercase font-serif text-center w-full  mb-8 mt-10 lg:mt-0 text-white-400 text-4xl">
+              Join Us on Our Journey
+            </h2>
+            <p className="p-4 font-sans font-white leading-9 text-white-400  text-2xl">
+              Whether you're a tech enthusiast, a fellow innovator, or someone
+              who simply enjoys the magic of AI and IoT, we invite you to join
+              us on this exciting journey. Explore our range of Arduino boards,
+              gadgets, and quirky creations. Dive into our articles and discover
+              how we're bringing the future to life one line of code at a time.
+            </p>
+          </div>
+          <div className="w-full lg:w-2/5">
+            <Lottie animationData={JoinAnimation} />
           </div>
         </section>
       </main>
