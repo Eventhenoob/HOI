476b8461584c9e321144ae74c2ec8ab0525f89eb
Even <cbhrat50@gmail.com>
2023-10-08 12:00:28 +0530

    Add disable effect to ArrowFloatingButton

--- src/components/ArrowFloatingButton/ArrowFloatingButton.tsx
+++ src/components/ArrowFloatingButton/ArrowFloatingButton.tsx
@@ -16,7 +16,11 @@ const ArrowFloatingButton = ({
       disabled={isDisabled}
       className={
         "rounded-full flex justify-center items-center text-3xl absolute top-0 translate-y-48 z-40 text-black bg-white-400 h-12 w-12 hover:scale-110 transition-transform duration-200 active:scale-95 " +
-        (direction === "left" ? "left-8" : "right-8")
+        (direction === "left" ? "left-8" : "right-8") +
+        " " +
+        (isDisabled
+          ? "!bg-slate-300 hover:scale-100 active:scale-100 cursor-not-allowed "
+          : "")
       }
       onClick={onClick}
     >
